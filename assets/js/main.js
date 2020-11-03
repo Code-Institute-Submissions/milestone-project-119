0cfeefd1150c3ef4e219fc8474633516d7a3de44
Gregory4321 <greg-lewis@hotmail.co.uk>
2020-11-03 16:22:14 +0000

    Inactive code removed. Pixels at where the navbar opacity changed to be lower

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
-//Get the button:
+/* ------ Nav bar opacity on scroll -------- */
+
+$(window).scroll(function() {
+    if($(this).scrollTop() > 120)  /*height in pixels when the navbar becomes non opaque*/ 
+    {
+        $('.navbar').addClass('navbar-on-scroll');
+    } else {
+        $('.navbar').removeClass('navbar-on-scroll');
+    }
+});
+
+/* ------ Back to top button -------- */
+
+//Get the button
+
 mybutton = document.getElementById("to-top-btn");
 
-// When the user scrolls down 60px from the top of the document, show the button
+// When the user scrolls down 200px from the top of the document, show the button
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -13,32 +28,8 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 };
-
-$(function() {
-
-$(document).on('click','a.page-scroll',function(event){
-  var $anchor=$(this);
-  $('html,body').stop().animate({
-    scrollTop: ($($anchor.attr('href')).offset().top-45)
-  },1500,'easeInOutExpo');
-  event.preventDefault();
-});
-});
-
-
-
-
-
-
- $(window).scroll(function() {
-    if($(this).scrollTop() > 100)  /*height in pixels when the navbar becomes non opaque*/ 
-    {
-        $('.navbar').addClass('navbar-on-scroll');
-    } else {
-        $('.navbar').removeClass('navbar-on-scroll');
-    }
-});
